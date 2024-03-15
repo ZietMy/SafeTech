7b33193383af98afeba099ff750079d8714461ce
Việt Mỹ <137087064+ZietMy@users.noreply.github.com>
2024-03-16 01:00:45 +0700

    My/layout client (#3)
    * layout-header
    
    * add footer1

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -7,6 +7,7 @@
     <title>{{$title}}</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/clients/css/layouts/footer.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/clients/css/bootstrap.min.css') }}">
     <style type="text/css">
         @yield('css');
@@ -17,14 +18,14 @@
     <main class="py-5">
         <div class="container">
             <div class="row">
-                <div class="col-4">
+                <div class="col-2">
                     <aside>
                         @section('sidebar')
                         @include('clients.blocks.sidebar')
                         @show
                     </aside>
                 </div>
-                <div class="col-8">
+                <div class="col-10">
                     <div class="content">
                         @yield('content')
                     </div>
