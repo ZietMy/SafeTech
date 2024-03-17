06b28035287cedf86925bcf712ffc588af76b0f2
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-03-17 21:05:29 +0700

    menu admin

---
+++ resources/views/admin/layouts/button.blade.php
@@ -0,0 +1,17 @@
+<!-- Actions -->
+<div class="col-sm-6 col-12 text-sm-end">
+    <div class="mx-n1">
+        <a href="#" class="btn d-inline-flex btn-sm btn-neutral border-base mx-1">
+            <span class=" pe-2">
+                <i class="bi bi-pencil"></i>
+            </span>
+            <span>Edit</span>
+        </a>
+        <a href="#" class="btn d-inline-flex btn-sm btn-primary mx-1">
+            <span class=" pe-2">
+                <i class="bi bi-plus"></i>
+            </span>
+            <span>Create</span>
+        </a>
+    </div>
+</div>
