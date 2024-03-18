ae664660256511e0f6637a5fffd632bd6ad6e9cd
Hi <hi.pham25@tudent.passerellesnumeriques.org>
2024-03-18 23:53:32 +0700

    Create user

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -10,7 +10,80 @@
             <div class="container-fluid">
                 <!-- Card stats -->
                 @include('admin.layouts.header-main')
-                @include('admin.layouts.main')
+                <div class="card shadow border-0 mb-7">
+                    <div class="table-responsive">
+                        <table class="table table-hover table-nowrap">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col">Avatar</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Role ID</th>
+                                    <th scope="col">PassWord</th>
+                                    <th scope="col">Address</th>
+                                    <th scope="col">Gender</th>
+                                    <th scope="col">Phone</th>
+                                    <th scope="col">Email</th>
+                                    {{-- <th scope="col">Status ID</th> --}}
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if(!empty($users))
+                                    @foreach ($users as $item)
+                                        <tr>
+                                            <td>
+                                                <img alt="..." src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80" class="avatar avatar-sm rounded-circle me-2">
+                                            </td>
+                                            <td>
+                                                <a class="text-heading font-semibold" href="#">
+                                                    {{$item->name}}
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a class="text-heading font-semibold" href="#">
+                                                    {{$item->role_id}}
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <a class="text-heading font-semibold" href="#">
+                                                    {{$item->password}}
+                                                </a>
+                                            </td>
+                                            <td>
+                                                {{$item->address}}
+                                            </td>
+                                            <td>
+                                                <a class="text-heading font-semibold" href="#">
+                                                    {{$item->gender}}
+                                                </a>
+                                            </td>
+                                            <td>
+                                                {{$item->phonenumber}}
+                                            </td>
+                                            <td>
+                                                {{$item->email}}
+                                            </td>
+                                            {{-- <td>
+                                                1
+                                            </td> --}}
+                                            <td class="text-end">
+                                                <a href="#" class="btn btn-sm btn-neutral">Edit</a>
+                                                <button type="button" class="btn btn-sm btn-square btn-neutral text-danger-hover">
+                                                    <i class="bi bi-trash"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td>Không có người dùng</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                            
+                        </table>
+                    </div>
+                </div>
             </div>
         </main>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DetailsController;
 use App\Http\Controllers\User\HomePageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,9 +24,8 @@ Route::get('/contact',[ContactController::class,'index'])->name('client.contact'
 Route::get('/admin', function(){
     return view('admin.layouts.admin');
 })->name('admin');
-Route::get('/admin/user', function(){
-    return view('admin.user');
-})->name('user');
+
+Route::get('/admin/user',[AdminController::class,'index'])->name('user');
 Route::get('/admin/product', function(){
     return view('admin.product');
 })->name('product');
