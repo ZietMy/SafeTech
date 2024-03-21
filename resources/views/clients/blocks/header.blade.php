b9bf5400f02da735269e51cc21f99eb78e7cbee3
Viet My <Ziettmy@gmail.com>
2024-03-22 00:34:35 +0700

    edit layout

--- resources/views/clients/blocks/header.blade.php
+++ resources/views/clients/blocks/header.blade.php
@@ -1,55 +1,39 @@
-<header class='py-2 border shadow'>
+<header class="header">
     <div class="container">
-        <div class='row d-flex justify-content-between align-items-center'>
-            <div class='col-2'>
-                <a class="navbar-brand" href="home">
-                    <img src="{{asset('assets/clients/images/logo.png')}}" alt="SafeTech"style="width:4rem;">
-                </a>
-            </div>
-            <div class='col-6 d-flex justify-content-center align-items-center'>
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <div class="header__inner">
+            <!-- Logo -->
+            <img src="{{asset('assets\clients\images\logo.png')}}" alt="SafeTech" class="logo" style="width:56px"/>
+
+            <!-- Navbar -->
+            <nav class="navbar">
+                <ul class="navbar__list">
+                    <li class="navbar__item">
+                        <a href="home" class="navbar__link">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Products</a>
+                    <li class="navbar__item">
+                        <a href="Products" class="navbar__link">products</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About us</a>
+                    <li class="navbar__item">
+                        <a href="#!" class="navbar__link">
+                            Services & Rates
+                        </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
+                    <li class="navbar__item">
+                        <a href="#!" class="navbar__link">Reviews</a>
                     </li>
-                    
+                    <li class="navbar__item">
+                        <a href="#!" class="navbar__link">
+                            Contacts us
+                        </a>
+                    </li>
                 </ul>
-            </div>
+            </nav>
 
-            <div class="col-2 d-flex justify-content-end align-items-center">
-                @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="#!">
-                            <i class="fas fa-cart-plus"></i>
-                        </a>
-
-                        {{-- <a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a> --}}
-                    @else
-                        <a href="{{ route('login') }}">
-                            <button type="button" class="btn btn-danger mx-1">Login</button>
-                        </a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">
-                                <button type="button" class="btn btn-light">Register</button>
-                            </a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-                
-               
+            <!-- Header action -->
+            <div class="header-action">
+                <a href="#!" class="header-action__link">Sign in</a>
+                <a href="#!" class="btn-btn header-action__btn">Sign up</a>
             </div>
         </div>
     </div>
-
 </header>
