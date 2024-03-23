3c4d90f3a61a279a4befd42923257f02837ce410
thu <thu.vo25@student.passerellesnumeriques.org>
2024-03-23 08:45:41 +0700

    admin category

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -12,4 +12,21 @@ class Categories extends Model
         $categories = DB::table('SELECT * FROM categories');
         return $categories;
     }
+    public function postCategories($data){
+        $categories = DB::table('categories')->insert($data);
+        return $categories;
+    }
+    public function getCategories($id) {
+        $category = DB::table('categories')->where('id', $id)->first();
+        return $category;
+    }
+    public function postEdit($data, $id) {
+        DB::table('categories')
+        ->where('id', $id)
+        ->update([
+            'name' => $data['name'],
+        ]);
+
+    return redirect()->back()->with('msg', 'Cập nhật thành công');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\User\HomePageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,19 +19,23 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::get('/',[HomeController::class,'index']);
+Route::get('/', [HomeController::class, 'index']);
 Route::get('/detail/{id}', [DetailsController::class, 'detailId'])->name('clients.detail');
-Route::get('/contact',[ContactController::class,'index'])->name('client.contact');
-Route::get('/admin', function(){
+Route::get('/contact', [ContactController::class, 'index'])->name('client.contact');
+Route::get('/admin', function () {
     return view('admin.blocks.card');
 })->name('admin');
-Route::get('/admin/user',[AdminController::class,'index'])->name('user');
-Route::get('/admin/product', function(){
+Route::get('/admin/user', [AdminController::class, 'index'])->name('user');
+Route::get('/admin/product', function () {
     return view('admin.product');
 })->name('product');
-Route::get('/admin/order', function(){
+Route::get('/admin/order', function () {
     return view('admin.order');
 })->name('order');
-Route::get('/admin/catogories', function(){
-    return view('admin.categories');
-})->name('categories');
+
+Route::get('/admin/categories', [CategoriesController::class, 'index'])->name('categories');
+
+Route::get('/admin/categories/create', [CategoriesController::class, 'getCategories'])->name('categories.create');
+Route::post('/admin/categories/create', [CategoriesController::class, 'postCategories'])->name('post-add');
+Route::get('/admin/categories/edit/{id}', [CategoriesController::class, 'editCategories'])->name('categories.edit');
+Route::post('/admin/categories/edit', [CategoriesController::class, 'postEditCategories'])->name('post-edit');
