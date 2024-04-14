2d616fdd4b870ce37f30ffe43b07b8d11802c1d1
thu <thu.vo25@student.passerellesnumeriques.org>
2024-04-14 21:07:42 +0700

    first

--- resources/views/clients/wishlist.blade.php
+++ resources/views/clients/wishlist.blade.php
@@ -7,20 +7,9 @@
 @endsection
 @section('content1')
     <div class="container">
-        <div class="row">
-            <div class="Frame727" style="width: 1170px; height: 103px; justify-content: flex-start; align-items: flex-end; gap: 470px; display: inline-flex">
-                <div class="Frame725" style="justify-content: flex-start; align-items: flex-end; gap: 87px; display: flex">
-                  <div class="Frame626" style="height: 103px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
-                    <div class="Frame625" style="justify-content: flex-start; align-items: center; gap: 16px; display: inline-flex">
-                      <div class="Rectangle18" style="width: 20px; height: 40px; position: relative">
-                        <div class="Rectangle17" style="width: 20px; height: 40px; left: 0px; top: 0px; position: absolute; background: #DB4444; border-radius: 4px"></div>
-                      </div>
-                      <div class="TodayS" style="color: #DB4444; font-size: 16px; font-family: Poppins; font-weight: 600; line-height: 20px; word-wrap: break-word">Your WishList</div>
-                    </div>
-                  </div>
-                </div>
-            </div>
-        </div>
+        <div class="container1">
+            <span class="text1">Your wishlist</span>
+          </div>
        @if( $wishlist->isNotEmpty())
        <table class="table table-hover">
            <thead>
