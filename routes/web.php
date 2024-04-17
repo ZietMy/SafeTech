0c17ec4b1cc53dccb04a7ad8a3d96c9f3da567da
vanthu1223 <137027421+vanthu1223@users.noreply.github.com>
2024-04-17 15:18:51 +0700

    inteface (#33)

--- routes/web.php
+++ routes/web.php
@@ -32,18 +32,20 @@ Route::get('/',  [HomeController::class, 'index'])->name('home');
 Route::get('/detail/{id}', [DetailsController::class, 'detailId'])->name('clients.detail');
 Route::get('/contact', [ContactController::class, 'index'])->name('client.contact');
 Route::get('/product', [ProductController::class, 'index'])->name('product');
+Route::post('/contact/post', [ContactController::class, 'getForm'])->name('post-message');
 
 Route::middleware('auth')->group(function () {
 Route::get('/wishlist', [WishListController::class, 'wishList'])->name('wishlist');
 Route::post('/wishlist/add', [WishListController::class, 'addWishList'])->name('add-wish-list');
 Route::get('/wishlist/delete/{id}', [WishListController::class, 'deleteWishList'])->name('delete-wish-list');
-Route::post('/contact/post', [ContactController::class, 'getForm'])->name('post-message');
 Route::get("cart",[CartController::class, 'index'])->name('cart');
 Route::post("cart/store",[CartController::class, 'addToCart'])->name('cart.store');
 Route::post('cart/increment/{cartId}', [CartController::class, 'increment'])->name('cart.increment');
 Route::post('cart/decrement/{cartId}', [CartController::class, 'decrement'])->name('cart.decrement');
 Route::get('/delete-cart/{cartId}', [CartController::class, 'deleteCart'])->name('cart.delete');
 Route::get('/delete-all-cart', [CartController::class, 'deleteAllCart'])->name('cart.deleteAll');
+
+Route::get('/checkout',[CheckoutController::class,'index'])->name('checkout');
 });
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/home', [AdminController::class, 'index'])->name('admin');
@@ -91,7 +93,5 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile/edit',[ProfileController::class,'getEdit'])->name('profile.getEdit');
 });
 
-
-Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 
 require __DIR__ . '/auth.php';
