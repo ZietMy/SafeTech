ef1ebdc17f918a639c10d6b10a21fb0e0eff76b0
thu <thu.vo25@student.passerellesnumeriques.org>
2024-04-17 14:02:51 +0700

    thu/profile

--- routes/web.php
+++ routes/web.php
@@ -85,9 +85,9 @@ Route::middleware(['auth','admin'])->prefix('admin')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile',[ProfileController::class,'index'])->name('profile.user');
+    Route::post('/profile/edit', [ProfileController::class, 'postEdit'])->name('profile.edit');
+    Route::get('/profile/edit',[ProfileController::class,'getEdit'])->name('profile.getEdit');
 });
 
 
